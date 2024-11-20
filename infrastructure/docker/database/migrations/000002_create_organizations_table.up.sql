025eaf589a8ea38afccb03a5890f6a65ad35c5c6
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-11-20 22:37:59 +0000

    add migration for organization

--- infrastructure/docker/database/migrations/000002_create_organizations_table.up.sql
+++ infrastructure/docker/database/migrations/000002_create_organizations_table.up.sql
@@ -1,20 +1,16 @@
-SET TIME ZONE 'UTC';
-
-CREATE TABLE users
+CREATE TABLE organizations
 (
     id            uuid PRIMARY KEY         DEFAULT gen_random_uuid(),
-    first_name    VARCHAR(255)        NOT NULL,
-    last_name     VARCHAR(255)        NULL,
-    email         VARCHAR(255) UNIQUE NOT NULL,
-    password      VARCHAR(255)        NOT NULL,
-    phone         VARCHAR(20)         NULL,
+    name          VARCHAR(255) NOT NULL,
+    owner_id      UUID         NOT NULL,
     created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     deleted_at    TIMESTAMP WITH TIME ZONE DEFAULT NULL,
-    created_by_id UUID                NULL,
-    updated_by_id UUID                NULL,
-    deleted_by_id UUID                NULL,
+    created_by_id UUID         NULL,
+    updated_by_id UUID         NULL,
+    deleted_by_id UUID         NULL,
 
+    CONSTRAINT fk_users_owner FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE RESTRICT,
     CONSTRAINT fk_users_created_by FOREIGN KEY (created_by_id) REFERENCES users (id) ON DELETE SET NULL,
     CONSTRAINT fk_users_updated_by FOREIGN KEY (updated_by_id) REFERENCES users (id) ON DELETE SET NULL,
     CONSTRAINT fk_users_deleted_by FOREIGN KEY (deleted_by_id) REFERENCES users (id) ON DELETE SET NULL
