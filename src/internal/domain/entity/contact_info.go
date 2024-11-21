9b6c248ae798d96c008554ab6b1235d6e1869ed1
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-11-21 20:46:39 +0000

    rename structure

---
+++ src/internal/domain/entity/contact_info.go
@@ -0,0 +1,6 @@
+package entity
+
+type ContactInfo struct {
+	Phone string `json:"phone"`
+	Email string `json:"email"`
+}
--- src/internal/domain/entity/user.go
+++ src/internal/domain/entity/user.go
@@ -1,20 +1,15 @@
 package entity
 
 type User struct {
-	ID        string    `json:"id"`
-	Password  string    `json:"password"`
-	Contacts  Contacts  `json:"contacts"`
-	Info      UserInfo  `json:"info"`
-	UserStamp UserStamp `json:"user_stamp"`
-	Timestamp Timestamp `json:"timestamp"`
+	ID        string      `json:"id"`
+	Password  string      `json:"password"`
+	Contacts  ContactInfo `json:"contacts"`
+	Info      UserInfo    `json:"info"`
+	UserStamp UserStamp   `json:"user_stamp"`
+	Timestamp Timestamp   `json:"timestamp"`
 }
 
 type UserInfo struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
-}
-
-type Contacts struct {
-	Phone string `json:"phone"`
-	Email string `json:"email"`
 }
