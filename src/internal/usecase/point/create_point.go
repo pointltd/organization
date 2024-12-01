5cde08ae179b3d31e1deffb01b8bad25a33b4522
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-12-01 00:22:09 +0000

    update timestamps

--- src/internal/usecase/point/create_point.go
+++ src/internal/usecase/point/create_point.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pointltd/organization/internal/domain/repository"
 	_def "github.com/pointltd/organization/internal/usecase"
 	"log/slog"
+	"time"
 )
 
 var _ _def.CreatePointUseCase = (*createPointUseCase)(nil)
@@ -26,6 +27,13 @@ func (u createPointUseCase) Execute(name string, organizationId string) (entity.
 		Name:           name,
 		OrganizationId: organizationId,
 	}
+
+	timestamps := entity.Timestamp{
+		CreatedAt: func(t time.Time) *time.Time { return &t }(time.Now()),
+		UpdatedAt: func(t time.Time) *time.Time { return &t }(time.Now()),
+	}
+
+	point.Timestamp = timestamps
 
 	point, err := u.pointRepository.Save(point)
 	if err != nil {
