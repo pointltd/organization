ff72b4079b92eb396d788691d4e9e79f60e0222d
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-12-08 13:06:05 +0000

    change log level

--- src/internal/infrastructure/http/controller/user/list_user_organizations.go
+++ src/internal/infrastructure/http/controller/user/list_user_organizations.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (c *controller) ListUserOrganizations(ctx echo.Context) error {
-	c.log.Debug("ListUserOrganizations invoked")
+	c.log.Info("ListUserOrganizations invoked")
 	users, err := c.listUserOrganizationsUseCase.Execute(
 		ctx.Param("id"),
 	)
