0d9047e4e70f348c8d690ab2563b234de64c113d
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-12-08 01:29:38 +0000

    fix it

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -54,13 +54,13 @@ resource "yandex_resourcemanager_folder_iam_member" "registry_pull_permission" {
   member    = "serviceAccount:${yandex_iam_service_account.organization-sa.id}"
 }
 
-resource "yandex_lockbox_secret_iam_binding" "lockbox_payload_viewer_permission" {
+resource "yandex_lockbox_secret_iam_binding" "lockbox_db_url_viewer_permission" {
     secret_id = local.db_url_secret_id
     role      = "lockbox.payloadViewer"
     members   = ["serviceAccount:${yandex_iam_service_account.organization-sa.id}"]
 }
 
-resource "yandex_lockbox_secret_iam_binding" "lockbox_payload_viewer_permission" {
+resource "yandex_lockbox_secret_iam_binding" "lockbox_jwt_secret_id_viewer_permission" {
   secret_id = local.jwt_secret_id
   role      = "lockbox.payloadViewer"
   members   = ["serviceAccount:${yandex_iam_service_account.organization-sa.id}"]
