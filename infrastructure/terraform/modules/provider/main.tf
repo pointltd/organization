96f29fccec930259a96e5063b0f26577e712bc49
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-12-15 14:51:51 +0000

    fix

--- infrastructure/terraform/modules/provider/main.tf
+++ infrastructure/terraform/modules/provider/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.90.0"
+    }
+  }
+}
+
 provider "yandex" {
   zone      = var.zone
   folder_id = var.folder_id
